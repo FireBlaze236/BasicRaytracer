e10b8fd3fac8f827423bb1a521a0fcdaff39e292
FireBlaze236 <saminps3@gmail.com>
2023-01-18 19:07:27 +0600

    Init

---
+++ BasicRaytracer/BasicRaytracer.cpp
@@ -0,0 +1,12 @@
+﻿// BasicRaytracer.cpp : Defines the entry point for the application.
+//
+
+#include "BasicRaytracer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
